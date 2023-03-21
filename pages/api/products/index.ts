3b92e1092f84ea4749df203b6772598ddd5728e3
TutusBOT <barte77pl@gmail.com>
2023-03-21 23:54:27 +0100

    chore: tets

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,11 +1,19 @@
 import Product from "@/models/product";
 import { connectMongo } from "@/utils/connectMongo";
+import { getSession } from "@auth0/nextjs-auth0";
 import { NextApiRequest, NextApiResponse } from "next";
+
+export const config = {
+	api: {
+		bodyParse: false,
+	},
+};
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
+	const session = await getSession(req, res);
 	if (req.method === "GET") {
 		try {
 			await connectMongo();
@@ -16,6 +24,12 @@ export default async function handler(
 		}
 	}
 	if (req.method === "POST") {
+		// if (!session || !session.user)
+		// 	return res.status(401).json({
+		// 		error: "not_authenticated",
+		// 		description:
+		// 			"The user does not have an active session or is not authenticated",
+		// 	});
 		try {
 			await connectMongo();
 			const product = await Product.create(req.body);
