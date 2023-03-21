05f4834fcee129eb39d2b0fe9d674adde8a11871
TutusBOT <barte77pl@gmail.com>
2023-03-21 23:54:06 +0100

    feat: add user api route

---
+++ pages/api/users/index.ts
@@ -0,0 +1,50 @@
+import User from "@/models/user";
+import { connectMongo } from "@/utils/connectMongo";
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default withApiAuthRequired(
+	async (req: NextApiRequest, res: NextApiResponse) => {
+		const xd = await getSession(req, res);
+		console.log(xd);
+		if (req.method === "GET") {
+			try {
+				await connectMongo();
+				const user = await User.findOne(req.body.email);
+				res.json(user);
+			} catch (error) {
+				res.json(error);
+			}
+		}
+		if (req.method === "POST") {
+			try {
+				await connectMongo();
+				const user = await User.create(req.body);
+				res.json(user);
+			} catch (error) {
+				res.json(error);
+			}
+		}
+		if (req.method === "PATCH") {
+			try {
+				await connectMongo();
+				const user = await User.findOneAndUpdate(req.body.email, req.body, {
+					upsert: true,
+					new: true,
+				});
+				res.json(user);
+			} catch (error) {
+				res.json(error);
+			}
+		}
+		if (req.method === "DELETE") {
+			try {
+				await connectMongo();
+				await User.findOneAndDelete(req.body.email);
+				res.status(204).send("");
+			} catch (error) {
+				res.json(error);
+			}
+		}
+	}
+);
