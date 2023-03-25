9d4e8bc42a14c6fe52b070d65edeed79338d5e88
TutusBOT <barte77pl@gmail.com>
2023-03-25 23:41:32 +0100

    feat: add header component

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import Cart from "./Cart";
+import Profile from "./Profile";
+
+const Navbar = () => {
+	return (
+		<header>
+			<nav>
+				<ul className="flex justify-end shadow-md py-2 px-4 gap-4">
+					<li>
+						<Cart />
+					</li>
+					<li>
+						<Profile />
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+export default Navbar;
