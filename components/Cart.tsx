f83b30605838bbd658177a3c65b90340ddb5b32c
TutusBOT <barte77pl@gmail.com>
2023-03-26 23:48:27 +0200

    feat: add link to cart page

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,13 +1,16 @@
 import { useAppStore } from "@/store";
+import Link from "next/link";
 
 const Cart = () => {
-	const cart = useAppStore((state) => state);
+	const cart = useAppStore((state) => state.cart);
 	return (
 		<div className="group relative">
-			<div>Cart</div>
-			<div className="hidden group-hover:block absolute">
-				{cart.cart ? "Check cart" : "Cart is empty"}
-			</div>
+			<Link href="/cart">
+				<div>Cart</div>
+				<div className="hidden group-hover:block absolute">
+					{cart.length ? "Check cart" : "Cart is empty"}
+				</div>
+			</Link>
 		</div>
 	);
 };
