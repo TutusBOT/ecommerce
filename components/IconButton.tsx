f40a18a8ed935a28153f86c6ee137dfca631f831
TutusBOT <barte77pl@gmail.com>
2023-03-26 23:49:00 +0200

    feat: add IconButton component

---
+++ components/IconButton.tsx
@@ -0,0 +1,29 @@
+interface IconButton {
+	children?: React.ReactNode;
+	type?: "button" | "submit" | "reset";
+	className?: string;
+	onClick?: () => void;
+	disabled?: boolean;
+}
+
+const IconButton = ({
+	children,
+	type,
+	className,
+	onClick,
+	disabled,
+}: IconButton) => {
+	return (
+		<button
+			type={type}
+			onClick={onClick}
+			className={`flex items-center justify-center gap-2 p-2 rounded-full transition-colors hover:bg-gray-400 hover:text-white ${
+				className ?? ""
+			}`}
+			disabled={disabled}
+		>
+			{children}
+		</button>
+	);
+};
+export default IconButton;
