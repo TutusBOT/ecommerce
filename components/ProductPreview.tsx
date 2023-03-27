15d112d4f8cf1094b6dc1af454a0d5920f7087a9
TutusBOT <barte77pl@gmail.com>
2023-03-27 23:32:49 +0200

    feat: add basic functionality

--- components/ProductPreview.tsx
+++ components/ProductPreview.tsx
@@ -1,20 +1,39 @@
+import { Product } from "@/models/product";
+import { useAppStore } from "@/store";
 import Image from "next/image";
 import Link from "next/link";
+import { MdAddShoppingCart } from "react-icons/md";
+import IconButton from "./IconButton";
 
 interface ProductPreview {
-	id: string;
-	image: string;
-	title: string;
-	price: number;
+	product: Product;
 }
 
-const ProductPreview = ({ id, image, title, price }: ProductPreview) => {
+const ProductPreview = ({ product }: ProductPreview) => {
+	const addToCart = useAppStore((state) => state.addToCart);
+
 	return (
-		<div>
-			<Link href={`/product/${id}`}>
-				<Image src={image} alt={title} width={100} height={100} />
-				<p>{title}</p>
-				<p>{price}</p>
+		<div className="w-full rounded-lg border-transparent border-[1px] transition-colors hover:border-gray-200 hover:shadow-lg group">
+			<Link
+				href={`/product/${product._id}`}
+				className="flex flex-col px-4 py-2"
+			>
+				<Image
+					src={product.image ?? "/products/placeholder.png"}
+					alt={product.title}
+					width={100}
+					height={100}
+				/>
+				<p>{product.title}</p>
+				<div className="flex items-center justify-between">
+					<p>{product.price}</p>
+					<IconButton
+						onClick={() => addToCart(product)}
+						className="opacity-0 transition-opacity group-hover:opacity-100"
+					>
+						<MdAddShoppingCart />
+					</IconButton>
+				</div>
 			</Link>
 		</div>
 	);
