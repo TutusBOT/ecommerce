05bfffb4c4734a6cfb4cfdff5b43d874f3cdaafe
TutusBOT <barte77pl@gmail.com>
2023-04-03 23:44:00 +0200

    fix: while adding product to cart user was redirected to another page

--- components/ProductPreview.tsx
+++ components/ProductPreview.tsx
@@ -3,9 +3,9 @@
 import { Product } from "@/models/product";
 import { useAppStore } from "@/store";
 import Image from "next/image";
-import Link from "next/link";
 import { MdAddShoppingCart } from "react-icons/md";
 import IconButton from "./IconButton";
+import { useRouter } from "next/navigation";
 
 interface ProductPreview {
 	product: Product;
@@ -13,12 +13,15 @@ interface ProductPreview {
 
 const ProductPreview = ({ product }: ProductPreview) => {
 	const addToCart = useAppStore((state) => state.addToCart);
+	const { push } = useRouter();
 
 	return (
-		<div className="group w-full rounded-lg border border-transparent transition-colors hover:border-gray-200 hover:shadow-lg">
-			<Link
-				href={`/product/${product._id}`}
-				className="flex flex-col px-4 py-2"
+		<div className="group relative w-full rounded-lg border border-transparent transition-colors hover:border-gray-200 hover:shadow-lg">
+			<div
+				className="flex cursor-pointer flex-col px-4 py-2"
+				onClick={() => {
+					push(`/product/${product._id}`);
+				}}
 			>
 				<Image
 					src={product.image ?? "/products/placeholder.png"}
@@ -29,14 +32,14 @@ const ProductPreview = ({ product }: ProductPreview) => {
 				<p>{product.title}</p>
 				<div className="flex items-center justify-between">
 					<p>{product.price}</p>
-					<IconButton
-						onClick={() => addToCart(product)}
-						className="opacity-0 transition-opacity group-hover:opacity-100"
-					>
-						<MdAddShoppingCart />
-					</IconButton>
 				</div>
-			</Link>
+			</div>
+			<IconButton
+				onClick={() => addToCart(product)}
+				className="absolute bottom-2 right-2 z-10 opacity-0 transition-opacity group-hover:opacity-100"
+			>
+				<MdAddShoppingCart />
+			</IconButton>
 		</div>
 	);
 };
