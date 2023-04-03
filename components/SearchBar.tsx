c94fc223230275567a229aafb777940f3e64dcb7
TutusBOT <barte77pl@gmail.com>
2023-04-03 23:44:13 +0200

    feat: add logic to SearchBar component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import { MdSearch } from "react-icons/md";
 import IconButton from "./IconButton";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
+	const { push } = useRouter();
+	const [query, setQuery] = useState("");
 	return (
 		<div className="flex rounded-lg border border-gray-400">
-			<input type="text" className="bg-transparent" />
-			<IconButton>
+			<input
+				type="text"
+				className="bg-transparent"
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+			<IconButton
+				onClick={() => {
+					push(`/search?title=${query}`);
+					setQuery("");
+				}}
+			>
 				<MdSearch />
 			</IconButton>
 		</div>
