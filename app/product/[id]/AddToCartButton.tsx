eb62e4fe432be11f6e7cb0803f723669b898ecb8
TutusBOT <barte77pl@gmail.com>
2023-04-10 23:53:32 +0200

    feat: change button ui

--- app/product/[id]/AddToCartButton.tsx
+++ app/product/[id]/AddToCartButton.tsx
@@ -2,15 +2,46 @@
 
 import { Product } from "@/models/product";
 import { useAppStore } from "@/store";
+import { useState } from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 import { toast } from "react-toastify";
+import z from "zod";
+
+const amountSchema = z.preprocess((n) => {
+	if (!n) return 1;
+	return parseInt(z.string().parse(n));
+}, z.number().positive());
 
 const AddToCartButton = ({ product }: { product: Product }) => {
+	const [amount, setAmount] = useState("1");
+
 	const addToCart = useAppStore((state) => state.addToCart);
 	const handleAddToCart = () => {
-		addToCart(product);
+		const parsedAmount = amountSchema.parse(amount);
+		if (!parsedAmount)
+			return toast.error("Provide appropriate number of items.");
+		for (let i = 0; i < parsedAmount; i++) {
+			addToCart(product);
+		}
 		toast.success(`Added ${product.title} to cart`);
 	};
 
-	return <button onClick={handleAddToCart}>add to cart</button>;
+	return (
+		<div className="flex gap-2 p-4">
+			<input
+				type="number"
+				min={1}
+				value={amount}
+				onChange={(e) => setAmount(e.target.value)}
+				className="w-8 outline-none"
+			/>
+			<button
+				onClick={handleAddToCart}
+				className="flex items-center gap-2 rounded-xl bg-green-600 px-6 py-2 text-white transition-colors hover:bg-green-800"
+			>
+				<MdAddShoppingCart /> Add to cart
+			</button>
+		</div>
+	);
 };
 export default AddToCartButton;
