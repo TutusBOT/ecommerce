b75e31ffe6a31722831ff9948f0509fff399dad6
TutusBOT <barte77pl@gmail.com>
2023-04-16 23:29:08 +0200

    feat: add icon and improve layout

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,10 +1,14 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import SignIn from "./SignIn";
+import { MdAccountCircle } from "react-icons/md";
 
 const Profile = () => {
 	return (
 		<div className="group relative">
-			<div>Your account</div>
+			<div className="flex flex-col">
+				<MdAccountCircle size={32} />
+				<p className="text-xs">Profile</p>
+			</div>
 			<div className="absolute hidden w-full bg-white group-hover:block">
 				<SignIn />
 			</div>
