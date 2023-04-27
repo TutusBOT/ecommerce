8b592f0794c7b6b7b30b0e5ede76dee62a0d81c5
TutusBOT <barte77pl@gmail.com>
2023-04-27 23:26:49 +0200

    feat: move from 0auth to next-auth

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,17 +1,42 @@
-import { useUser } from "@auth0/nextjs-auth0/client";
-import SignIn from "./SignIn";
+"use client";
+
 import { MdAccountCircle } from "react-icons/md";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Profile = () => {
+	const { data: session } = useSession();
+
+	const handleProfileClick = () => {
+		if (session?.user) {
+			// redirect to profile page
+		} else {
+			signIn();
+		}
+	};
+
+	const handleButtonClick = () => {
+		if (session?.user) {
+			signOut();
+		} else {
+			signIn();
+		}
+	};
+
 	return (
 		<div className="group relative">
-			<div className="flex cursor-pointer flex-col">
+			<div
+				className="flex cursor-pointer flex-col"
+				onClick={handleProfileClick}
+			>
 				<MdAccountCircle size={32} />
 				<p className="text-xs">Profile</p>
 			</div>
-			<div className="absolute left-1/2 hidden w-20 -translate-x-1/2 rounded-lg border-[1px] border-gray-300 bg-white p-2 text-center text-sm group-hover:block">
-				<SignIn />
-			</div>
+			<button
+				className="absolute left-1/2 hidden w-20 -translate-x-1/2 rounded-lg border-[1px] border-gray-300 bg-white p-2 text-center text-sm group-hover:block"
+				onClick={handleButtonClick}
+			>
+				{session?.user ? "Sign out" : "Sign in"}
+			</button>
 		</div>
 	);
 };
