cdb438b055835d98c5d671a8e2368313b5c11620
TutusBOT <barte77pl@gmail.com>
2023-04-29 23:22:04 +0200

    feat: add hover effect

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,13 +2,15 @@
 
 import { MdAccountCircle } from "react-icons/md";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Profile = () => {
+	const { push } = useRouter();
 	const { data: session } = useSession();
 
 	const handleProfileClick = () => {
 		if (session?.user) {
-			// redirect to profile page
+			push("/profile");
 		} else {
 			signIn();
 		}
@@ -28,11 +30,15 @@ const Profile = () => {
 				className="flex cursor-pointer flex-col"
 				onClick={handleProfileClick}
 			>
-				<MdAccountCircle size={32} />
+				{session?.user?.image ? (
+					JSON.stringify(session.user.image)
+				) : (
+					<MdAccountCircle size={32} />
+				)}
 				<p className="text-xs">Profile</p>
 			</div>
 			<button
-				className="absolute left-1/2 hidden w-20 -translate-x-1/2 rounded-lg border-[1px] border-gray-300 bg-white p-2 text-center text-sm group-hover:block"
+				className="absolute left-1/2 hidden w-20 -translate-x-1/2 rounded-lg border-[1px] border-gray-300 bg-white p-2 text-center text-sm transition-colors hover:bg-gray-100 group-hover:block"
 				onClick={handleButtonClick}
 			>
 				{session?.user ? "Sign out" : "Sign in"}
