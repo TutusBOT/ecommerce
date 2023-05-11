6c2fdf26c98c3c8448696bcf3cac59476f98f774
TutusBOT <barte77pl@gmail.com>
2023-05-11 23:32:22 +0200

    fix: import react

--- app/SessionProvider.tsx
+++ app/SessionProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import React from "react";
 
 const Provider = ({
 	children,
@@ -8,7 +9,5 @@ const Provider = ({
 }: {
 	children: React.ReactNode;
 	session: any;
-}) => {
-	return <SessionProvider session={session}>{children}</SessionProvider>;
-};
+}) => <SessionProvider session={session}>{children}</SessionProvider>;
 export default Provider;
