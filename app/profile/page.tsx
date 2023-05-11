0d21634ce2676a9f6ae57881658fbb18603497bf
TutusBOT <barte77pl@gmail.com>
2023-05-11 12:25:03 +0200

    feat: add styling

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,8 +5,8 @@ import SignoutButton from "./SignoutButton";
 const page = async () => {
 	const session = await getServerSession(authOptions);
 	return (
-		<div>
-			{session?.user?.name}
+		<div className="flex flex-col items-center justify-center gap-8 pt-24">
+			<h2 className="text-4xl">Welcome, {session?.user?.email}</h2>
 			{session?.user ? <SignoutButton /> : ""}
 		</div>
 	);
