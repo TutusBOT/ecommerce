2f6e1f5124b378637df17ba58475253e7fc4f0cf
TutusBOT <barte77pl@gmail.com>
2023-05-29 23:46:56 +0200

    feat: improve responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import ProductModel, { Product } from "@/models/product";
-import ProductPreview from "@/components/ProductPreview";
+import ProductPreview from "@/components/ProductPreview/ProductPreview";
 import { connectMongo } from "@/lib/connectMongo";
 import FeaturedProduct from "@/components/FeaturedProduct/FeaturedProduct";
 
@@ -20,7 +20,7 @@ export default async function Home() {
 		<main className="flex flex-col items-center justify-center px-4">
 			<FeaturedProduct product={products[1]} />
 			<h2 className="mt-8 text-2xl">Recommended</h2>
-			<div className="grid w-full max-w-screen-2xl grid-cols-4 items-center justify-items-center gap-4 py-4">
+			<div className="grid w-full max-w-screen-2xl grid-cols-2 items-center justify-items-center gap-4 py-4 sm:grid-cols-3 lg:grid-cols-4">
 				{products.map((product: Product) => (
 					<ProductPreview key={product._id} product={product} />
 				))}
