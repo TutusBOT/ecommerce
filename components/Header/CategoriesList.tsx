d606f0a9b36cdc76a1d65f32aa36f6e34ca14b62
TutusBOT <barte77pl@gmail.com>
2023-06-02 02:50:46 +0200

    fix: positioning

--- components/Header/CategoriesList.tsx
+++ components/Header/CategoriesList.tsx
@@ -10,7 +10,7 @@ const CategoriesList = ({ categories }: { categories: Category[] }) => {
 	const [show, setShow] = useState(false);
 
 	return (
-		<div className="relative flex items-center justify-between">
+		<div className="relative z-10 flex items-center justify-between">
 			<Button
 				type="button"
 				className="my-2 block sm:hidden"
@@ -20,14 +20,14 @@ const CategoriesList = ({ categories }: { categories: Category[] }) => {
 				Categories
 			</Button>
 			<ul
-				className={`absolute -left-4 flex flex-col gap-4 rounded-br-xl  bg-gray-100 pb-2 pr-2 pt-1 transition-opacity sm:static sm:flex-row sm:rounded-none sm:pb-0 sm:pr-0 ${
+				className={`absolute -left-4 top-14 flex flex-col gap-4 rounded-br-xl  bg-gray-100 pb-2 pr-2 pt-1 transition-opacity sm:static sm:flex-row sm:rounded-none sm:pb-0 sm:pr-0 ${
 					show ? "flex" : "hidden sm:flex"
 				}`}
 			>
 				{categories.map((category) => (
 					<li
 						key={category.slug}
-						className="h-full rounded-t-lg border-0 border-gray-100  py-2 px-4 transition-all hover:border-x-gray-400 hover:border-t-gray-400 hover:bg-white hover:shadow-lg sm:border-[1px]"
+						className="h-full border-0 border-gray-100 py-2  px-4 transition-all hover:border-x-gray-400 hover:border-t-gray-400 hover:bg-white hover:shadow-lg sm:rounded-t-lg sm:border-[1px]"
 					>
 						<Link href={`/category/${category.slug}`}>{category.name}</Link>
 					</li>
